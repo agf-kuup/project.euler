ce763da66d49ffb49334d0e7927530d717e19a2b
Antonio González <antonio.gonzalez@springlabs.net>
2020-02-23 15:16:06 -0600

    Solutions switched between p6 & p9

--- 0009.py
+++ 0009.py
@@ -1,1 +1,7 @@
-print(abs(sum(map(lambda x: x**2,range(100)))-(sum(range(100)))**2))
+u=[0,0,0,0]
+for i in range(1,1000):
+    for j in range(1,1000-i+1):
+            if (1000-j-i)**2+j**2==i**2:
+                if i*j*(1000-i-j)> u[3]:
+                    u=[i,j,1000-i-j,i*j*(1000-i-j)]
+print(u[3])
