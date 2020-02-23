557bb7f68f3c4e43d32b673408d23aedb6a87641
Antonio González <antonio.gonzalez@springlabs.net>
2020-02-23 13:37:31 -0600

    less lines

--- 0004.py
+++ 0004.py
@@ -1,8 +1,7 @@
 m=0
 for i in range(101,999):
     for j in range(101,999):
-        u=str(i*j)
-        if u==u[::-1]:
+        if str(i*j)==str(i*j)[::-1]:
             if i*j>m:
                 m=i*j
 print(m)
