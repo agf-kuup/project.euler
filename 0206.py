dbd845d57e228b0cf183a25ee6b695183754af11
coder.generico <agf0710@gmail.com>
2020-04-01 21:34:54 -0600

    0206.py corrected without UTF8 accents

--- 0206.py
+++ 0206.py
@@ -1,8 +1,8 @@
 #Puede reducirse a hallar 
-for i in range(101010000+3,14*(10**7),2):
-    if i%10==3 or i%10==7: #El cuadrado termina en 9 y sólo puede ser de decena 3 o 7
+for i in range(101010000+3,14*(10**7),2):    
+    if i%10==3 or i%10==7: #El cuadrado termina en 9 y solo puede ser de decena 3 o 7
         n=(i**2)//100
-        if n%10 - 8 ==0: #El resto es fuerza bruta buscando el número
+        if n%10 - 8 ==0: #El resto es fuerza bruta buscando el numero
             n//=100
             if n%10 - 7 ==0: 
                 n//=100
