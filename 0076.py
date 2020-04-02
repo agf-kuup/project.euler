f7c0a00161eadfaa21a7e6669551f92c46d08718
coder.generico <agf0710@gmail.com>
2020-04-02 14:55:02 -0600

    Problems obtained from older

--- 0076.py
+++ 0076.py
@@ -2,12 +2,12 @@ import numpy as np
 n=101
 t=np.poly1d([1])
 for i in range(1,n):
-    s=[0 for z in range(n)]
+    s=[0 for z in range(n)]   # Coefs of the polynomial.
     for j in range(n):
-        if (j)%i==0:
-            s[j]=1
+        if (j)%i==0:          # Those coefs will only be non-zero if the exp isn't divisible by the number
+            s[j]=1            # It will be 1 IOC
     print(" ")
     print(np.poly1d(s[::-1]))
     print(" ")
-    t*=np.poly1d(s[::-1])
+    t*=np.poly1d(s[::-1])     # Since poly1D makes the polynomial ([1,2,3]) into x^2+2x+3, I need to reverse the array.
 print(t.coef[100])
--- 0077.py
+++ 0077.py
@@ -1,13 +1,22 @@
 import numpy as np
-n=101
+
+n=100 # I tried to verify if it was correct for the small numbers, but the solution was shorter than expected
+era =[1] * n
+primes=[]
+for p in range(2, n):
+    if era[p]:
+        primes.append(p)
+        for i in range(p*p, n, p):
+            era[i] = False
+
 t=np.poly1d([1])
-for i in range(1,n):
-    s=[0 for z in range(n)]
-    for j in range(n):
+for i in primes:
+    coefs=[0 for z in range(n+1)]
+    for j in range(n+1):
         if (j)%i==0:
-            s[j]=1
-    print(" ")
-    print(np.poly1d(s[::-1]))
-    print(" ")
-    t*=np.poly1d(s[::-1])
-print(t.coef[100])
+            coefs[j]=1
+    t*=np.poly1d(coefs[::-1])
+v=0
+while t.coef[v]<5000:
+    v+=1
+print(t.coef[v],v)
