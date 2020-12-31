4f75d5c8deb9c03e8cc6e6ea7c95b81f3fd800f0
agf-nohchil <61362029+iam-agf@users.noreply.github.com>
2020-12-31 14:35:16 +0100

    Create 0091.py
    Just an attempt and was correct.

---
+++ 0091.py
@@ -0,0 +1,18 @@
+t = 0
+d = 50
+d += 1
+
+for x1 in range(0,d):
+    for y1 in range(0,d):
+        for x2 in range(0,d):
+            for y2 in range(0,d):
+                a = x1**2 + y1**2
+                b = x2**2 + y2**2
+                c = (x1-x2)**2 + (y1-y2)**2
+                if a+b==c or a+c==b or b+c==a:
+                    if (x1,y1)!=(x2,y2) and (x1,y1)!=(0,0) and (x2,y2)!=(0,0):                        
+                        t += 1
+t -= 1 # Removes the (0,0),(0,0),(0,0) triangle.
+t //= 2 # Removes symmetrical triangles.
+t += 1 # Adds something I still don't understand.
+print(t)
